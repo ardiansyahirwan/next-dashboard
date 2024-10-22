c00bc9dfeccb2cb40932dd484f2d3a039d861cdc
ardiansyahirwan <ardiansyahirwan923@gmail.com>
2024-10-22 12:51:41 +0700

    chore:  edit a to link

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -31,7 +31,7 @@ export default function NavLinks() {
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
             className={ clsx("flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm hover:bg-sky-100 hover:text-blue-700 md:flex-none md:justify-start md:p-2 md:px-3",
@@ -42,7 +42,7 @@ export default function NavLinks() {
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
